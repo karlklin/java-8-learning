d097396bfbca1b66fe099a3fa02e844036734937
Karol Pawelski <karol.pawelski@cognifide.com>
2017-12-12 12:20:40 +0100

    FilterCollection

---
+++ src/test/java/com/technologyconversations/java8exercises/FilterCollectionSpec.java
@@ -0,0 +1,30 @@
+package com.technologyconversations.java8exercises;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+Filter collection so that only elements with less then 4 characters are returned.
+ */
+public class FilterCollectionSpec {
+
+    private FilterCollection filterCollection;
+
+    @BeforeEach
+    void setUp() {
+        filterCollection = new FilterCollection();
+    }
+
+    @Test
+    void transformShouldFilterCollection() {
+        List<String> collection = asList("My", "name", "is", "John", "Doe");
+        List<String> expected = asList("My", "is", "Doe");
+        assertThat(filterCollection.transform(collection)).isEqualTo(expected);
+    }
+
+}
